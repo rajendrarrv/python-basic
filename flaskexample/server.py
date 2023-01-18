2ab7b056786705991eeb9ce006f9e280f94d9368
rajendrarrv <rajendrarajaramv@gmail.com>
2023-01-18 11:07:05 +0530

    Flask Routing 1. simple routing 2. routing with product name 3. routing multiple arguments
    https://pythonbasics.org/flask-tutorial-routes/
     On branch flask-template
     Changes to be committed:
    	modified:   flaskexample/server.py

--- flaskexample/server.py
+++ flaskexample/server.py
@@ -10,4 +10,17 @@ def index():
     users = [ 'Rosalia','Adrianna','Victoria' ]
     return render_template('index.html', title='Welcome', members=users)
 
+@app.route('/hello')
+def hello_world():
+   return "hello world"
+#  with name    
+@app.route('/product/<name>')
+def get_product(name):
+  return "The product is " + str(name)
+# flask route multiple arguments
+@app.route('/create/<first_name>/<last_name>')
+def create(first_name=None, last_name=None):
+  return 'Hello ' + first_name + ',' + last_name
+
+
 app.run(host='0.0.0.0')
