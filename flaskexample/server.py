996dad8a43a018f5d47ccd2d7fbdd0481baee982
rajendrarrv <rajendrarajaramv@gmail.com>
2023-01-18 11:23:01 +0530

    flask route post
    https://pythonbasics.org/flask-tutorial-routes/
    
     On branch flask-rout
     Changes to be committed:
    	modified:   flaskexample/server.py
    	new file:   flaskexample/templates/login.html

--- flaskexample/server.py
+++ flaskexample/server.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
+from flask import Flask, redirect, url_for, request
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -22,5 +26,20 @@ def get_product(name):
 def create(first_name=None, last_name=None):
   return 'Hello ' + first_name + ',' + last_name
 
+@app.route('/dashboard/<name>')
+def dashboard(name):
+   return 'welcome %s' % name
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['name']
+      return redirect(url_for('dashboard',name = user))
+   else:
+      user = request.args.get('name')
+      return render_template('login.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
 
 app.run(host='0.0.0.0')
