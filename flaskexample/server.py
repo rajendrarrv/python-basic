e49554046814b92ad61c6a08649ef926b8d7bc8f
rajendrarrv <rajendrarajaramv@gmail.com>
2023-01-24 20:00:55 +0530

    Flask SQLite database
    https://pythonbasics.org/flask-sqlite/
    
     On branch flask-database
     Changes to be committed:
    	new file:   flaskexample/database.db
    	modified:   flaskexample/server.py
    	new file:   flaskexample/templates/list.html
    	modified:   flaskexample/templates/result.html
    	modified:   flaskexample/templates/student.html

--- flaskexample/server.py
+++ flaskexample/server.py
@@ -1,26 +1,54 @@
 from flask import Flask, make_response, request, render_template, redirect, url_for
+import sqlite3
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-   return render_template('index.html')
+@app.route('/enternew')
+def new_student():
+   return render_template('student.html')
 
-@app.route('/setcookie', methods = ['POST', 'GET'])
-def setcookie():
+
+
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
    if request.method == 'POST':
-    user = request.form['nm']
-   
-   resp = make_response(render_template('readcookie.html'))
-   resp.set_cookie('userID', user)
-   
-   return resp
-@app.route('/getcookie')
-def getcookie():
-   name = request.cookies.get('userID')
-   return '<h1>welcome ' + name + '</h1>'
+      try:
+         nm = request.form['nm']
+         addr = request.form['add']
+         city = request.form['city']
+         pin = request.form['pin']
+         
+         with sqlite3.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
+            
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+      
+      finally:
+         return render_template("result.html", msg =msg)
+         con.close()
 
 
+@app.route('/list')
+def list():
+    con = sqlite3.connect("database.db")
+    con.row_factory = sqlite3.Row
 
+    cur = con.cursor()
+    cur.execute("select * from students")
+
+    rows = cur.fetchall();
+    return render_template("list.html", rows=rows)
 if __name__ == '__main__':
-   app.run(debug = True)
+  conn = sqlite3.connect('database.db')
+  print ("Opened database successfully");
+  conn.execute('CREATE TABLE  IF NOT EXISTS students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
+  print ("Table created successfully");
+  conn.close()
+app.run(debug = True)
